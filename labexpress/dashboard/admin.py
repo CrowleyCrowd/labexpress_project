b4fbafa5c30c71cc30f6b4acb6571f4daddcc045
Efrenn Alba <efrennap_023@hotmail.com>
2023-01-30 01:59:11 -0500

    Cambios: - Se define diseño base de las ventanas - Se crean las pantallas principales - Enlaces a las diferentes pantallas desde el sidebar. - Creación de estilos CSS - Carga de logotipos SVG - Cambios Estructura base de datos

--- labexpress/dashboard/admin.py
+++ labexpress/dashboard/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Profile
+from .models import Customer
+from .models import Category
+from .models import Brand
+from .models import Product
+from .models import Device
+from .models import Order
 
 # Register your models here.
+
+admin.site.register(Profile)
+admin.site.register(Customer)
+admin.site.register(Category)
+admin.site.register(Brand)
+admin.site.register(Product)
+admin.site.register(Device)
+admin.site.register(Order)
---
+++ labexpress/labexpress/static/assets/fonts/team.init.js
@@ -0,0 +1,42 @@
+﻿var buttonGroups, list = document.querySelectorAll(".team-list");
+function onButtonGroupClick(e) {
+    "list-view-button" === e.target.id || "list-view-button" === e.target.parentElement.id ? (document.getElementById("list-view-button").classList.add("active"),
+        document.getElementById("grid-view-button").classList.remove("active"),
+        Array.from(list).forEach(function (e) {
+            e.classList.add("list-view-filter"),
+                e.classList.remove("grid-view-filter")
+        })) : (document.getElementById("grid-view-button").classList.add("active"),
+            document.getElementById("list-view-button").classList.remove("active"),
+            Array.from(list).forEach(function (e) {
+                e.classList.remove("list-view-filter"),
+                    e.classList.add("grid-view-filter")
+            }))
+}
+list && (buttonGroups = document.querySelectorAll(".filter-button")) && Array.from(buttonGroups).forEach(function (e) {
+    e.addEventListener("click", onButtonGroupClick)
+});
+var url = "assets/json/", allmemberlist = "";
+var searchMemberList = document.getElementById("searchMemberList");
+searchMemberList.addEventListener("keyup", function () {
+    var e = searchMemberList.value.toLowerCase();
+    t = e;
+    var t, e = allmemberlist.filter(function (e) {
+        return -1 !== e.Name.toLowerCase().indexOf(t.toLowerCase()) || -1 !== e.Address.toLowerCase().indexOf(t.toLowerCase())
+    });
+    0 == e.length ? (document.getElementById("noresult").style.display = "block",
+        document.getElementById("teamlist").style.display = "none") : (document.getElementById("noresult").style.display = "none",
+            document.getElementById("teamlist").style.display = "block"),
+        loadTeamData(e)
+});
+function loadTeamData(result) {
+    let items = document.querySelectorAll("#team-member-list>div.col");
+    items.forEach(function (e, t) {
+        let id = e.id;
+        var item = result.filter(x => x.IdeCompany == id);
+        if (item.length == 0) {
+            e.style.display = "none";
+        } else {
+            e.style.display = "";
+        }
+    })
+}
