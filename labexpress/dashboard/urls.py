675a910a764dc1f1e2acd22aa1a270ac3cb5621e
CrowleyCrowd <efrennap_023@hotmail.com>
2023-02-04 11:51:43 -0500

    Cambios: - Creacion de Rutas para plantillas Gabriel y Daniel. - Creacion de Plantillas create_user, create_device

--- labexpress/dashboard/urls.py
+++ labexpress/dashboard/urls.py
@@ -10,8 +10,10 @@ from django.shortcuts import render, redirect
 urlpatterns = [
     #path('login/', views.login, name='login'),
     path('repairs/', login_required(views.repairs), name='repairs'),
+    path('devices/create_device', login_required(views.createDevices), name='create_new_device'),
     path('devices/', login_required(views.devices), name='devices'),
     path('customers/', login_required(views.customers), name='customers'),
+    path('users/create_user', login_required(views.createUser), name='create_new_user'),
     path('users/', login_required(views.users), name='users'),
     path('', login_required(views.index), name='index'),
     path('',login,{'template_name':'login.html'}, name = 'login')
