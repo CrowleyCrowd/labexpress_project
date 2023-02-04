4a7c1c6e546357a2394fd762018218b3c41b6fe7
CrowleyCrowd <efrennap_023@hotmail.com>
2023-02-04 11:00:09 -0500

    Cambios: - Implementación inicio de sesión - Implementación requiere inicio de sesión en páginas

--- labexpress/dashboard/urls.py
+++ labexpress/dashboard/urls.py
@@ -1,12 +1,18 @@
 from django.urls import path
 
 from . import views
+from django import forms
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('users/', views.users, name='users'),
-    path('customers/', views.customers, name='customers'),
-    path('devices/', views.devices, name='devices'),
-    path('repairs/', views.repairs, name='repairs'),
-    
+    #path('login/', views.login, name='login'),
+    path('repairs/', login_required(views.repairs), name='repairs'),
+    path('devices/', login_required(views.devices), name='devices'),
+    path('customers/', login_required(views.customers), name='customers'),
+    path('users/', login_required(views.users), name='users'),
+    path('', login_required(views.index), name='index'),
+    path('',login,{'template_name':'login.html'}, name = 'login')
 ]
