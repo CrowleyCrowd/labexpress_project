596f36e3446c555c11c6a7a4f13761b200cff2e0
Efrenn Alba <efrennap_023@hotmail.com>
2023-02-09 03:41:34 -0500

    Cambios - Se define formato para agregar orden. - Se agrega funcion de editar y eliminar orden.

--- labexpress/dashboard/views.py
+++ labexpress/dashboard/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-
+from django.core.exceptions import ObjectDoesNotExist
 from .forms import CustomerForm, RepairForm
 from .models import Customer, Repair
 
@@ -51,7 +51,7 @@ def CreateDevice(request):
 
 
 def Repairs(request):
-    repairs = Repair.objects.all()
+    repairs = Repair.objects.filter(status=True)
     return render(request, 'dashboard/repairs.html', {
         'repairs': repairs
     })
@@ -66,6 +66,33 @@ def CreateRepair(request):
     else:
         repair_form = RepairForm()
     return render(request, 'dashboard/create_repair.html', {'repair_form': repair_form})
+
+
+def EditRepair(request, id):
+    repair_form = None
+    error = None
+    try:
+        editRepair = Repair.objects.get(id=id)
+        if request.method == 'GET':
+            repair_form = RepairForm(instance=editRepair)
+        else:
+            repair_form = RepairForm(request.POST, instance=editRepair)
+            if repair_form.is_valid():
+                repair_form.save()
+            return redirect('repairs')
+    except ObjectDoesNotExist as e:
+        error = e
+
+    return render(request, 'dashboard/create_repair.html', {'repair_form': repair_form, 'error': error})
+
+
+def DeleteRepair(request, id):
+    delete_repair = Repair.objects.get(id=id)
+    if request.method == 'POST':
+        delete_repair.status = False
+        delete_repair.save()
+        return redirect('repairs')
+    return render(request, 'dashboard/delete_repair.html', {'delete_repair': delete_repair})
 
 
 def CreateDevices(request):
